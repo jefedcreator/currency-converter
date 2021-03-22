10da83371111aa63a0fc3572ef7d3201e67ab13b
jefedcreator <smallztin@gmail.com>
2021-03-22 16:03:18 +0100

    Done

---
+++ src/CurrencyRow.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function CurrencyRow({
+  currencyOptions,
+  selectedCurrency,
+  onChangeCurrency,
+  onChangeAmount,
+  amount
+}) {
+
+  return (
+    <div>
+      <input type="number" className="input" value={amount} onChange={onChangeAmount} />
+      <select value={selectedCurrency} onChange={onChangeCurrency}>
+        {currencyOptions.map(option => (
+          <option key={option} value={option}>{option}</option>
+        ))}
+      </select>
+    </div>
+  )
+}
---
+++ src/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Header (){
+    return (
+        <h1> Currency Converter App </h1>
+    )
+};
